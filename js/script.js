dd7fe843d1f52606f3f929bbe05b3b969b21a509
BuildTools <unconfigured@null.spigotmc.org>
2018-05-14 16:50:36 -0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,32 @@
+$(function(){
+  $("#atm_scr").hide();
+});
+
+$("#submit_btn").click(function() {
+  $("#login_scr").hide();
+  $("#atm_scr").show();
+});
+
+$("#consultar_btn").click(function() {
+
+});
+
+$("#extraer_btn").click(function() {
+
+});
+
+$("#ver_btn").click(function() {
+
+});
+
+$("#transferir_btn").click(function() {
+
+});
+
+$("#depositar_btn").click(function() {
+
+});
+
+$("#salir_btn").click(function() {
+
+});
