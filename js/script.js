a2b947ae0c1b0860f00b4ee6b3f8c564da68a4dd
Joaquín León <32375684+JoaquinLeonArg@users.noreply.github.com>
2018-06-14 21:40:21 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,19 +1,245 @@
-screens = ["#consultar", "#extraer"]
+screens = ["#consultar", "#extraer", "#ultimos", "#transferir", "#depositar", "#salir"]
 
-$(function() {
-  $("#atm_scr").hide();
+$(document).ready(function() {
+  $("#atm_scr").hide()
+  $("#consultar_scr").hide()
+  $("#extraer_scr").hide()
+  $("#ultimos_scr").hide()
+  $("#transferir_scr").hide()
+  $("#depositar_scr").hide()
+  $("#salir_scr").hide()
+  $("#loading").hide()
   screens.forEach(function(e) {
-    $(e + "_scr").hide();
     $(e + "_btn").click(function() {
       screens.forEach(function(o) {
-        $(o + "_scr").hide();
-      });
-      $(e + "_scr").show();
-    });
-  });
-});
+        $(o + "_scr").hide()
+      })
+      $(e + "_scr").show()
+    })
+  })
+})
+
+
 
 $("#submit_btn").click(function() {
-  $("#login_scr").hide();
-  $("#atm_scr").show();
-});
+  cliente_id = $("#user_id").val()
+
+  $.ajax({
+    url: "http://www.mocky.io/v2/5b22511f2e00006500e31619",
+    type: "GET",
+    beforeSend: function() {
+      $("<div/>", {class: "progress", id: "loading_bar"}).insertAfter($("#submit_btn"))
+      $("#loading_bar").append($("<div/>", {class: "indeterminate"}))
+      $("#submit_btn").addClass("disabled")
+      $("#user_id").prop("disabled", true)
+      $("#user_pwd").prop("disabled", true)
+    },
+    success: function(response) {
+        cliente = response
+        $("#login_scr").hide()
+        $("#atm_scr").show()
+        $("#loading_bar").remove()
+    }
+    })
+
+})
+
+
+// ------------------------------------------- EXTRACCION ------------------------------------------------//
+$("#confirmarextraccion_btn").click(function() {
+  creacion_movimiento = Math.floor(Date.now() / 1000)
+  importe = $("#cantidadextraer_txt").val()
+  numero_cuenta = $("#cuentaextraer_txt").val()
+
+  if (!$.isNumeric(importe) || !$.isNumeric(numero_cuenta)) {
+    $("#extraer_estado").text("El número contiene caracteres no-numericos.")
+    return
+  }
+
+  $.ajax({
+    url: "http://www.mocky.io/v2/5b2253032e00009100e3162b",
+    type: "GET",
+    beforeSend: function() {
+      $("#extraer_estado").text("Cargando...")
+    },
+    success: function(response){
+        console.log(response)
+        cuenta = response
+
+        if (cuenta["cliente_id"] !== cliente_id) {
+          $("#extraer_estado").text("Número de cuenta incorrecto.")
+          return
+
+        }
+    }
+    })
+  $.ajax({
+    url: "http://www.mocky.io/v2/5b2253412e00002a00e3162f",
+    type: "GET",
+    beforeSend: function()   {
+      $("#extraer_estado").text("Cargando...")
+    },
+    success: function(response){
+        console.log(response)
+        saldo_cuenta = response
+
+        if (importe > saldo_cuenta) {
+          $("#extraer_estado").text("Saldo insuficiente.")
+          return
+        }
+
+        movimiento = {"creado": creacion_movimiento,
+        "procesado": Math.floor(Date.now() / 1000),
+        "tipo": 0,
+        "estado": 1,
+        "importe": importe,
+        "id_cuenta": numero_cuenta}
+
+        $.ajax({
+          url: "http://www.mocky.io/v2/5b2253622e00007b00e31630",
+          data: movimiento,
+          type: "POST",
+          success: function(response){
+              console.log(response)
+              $("#extraer_estado").text("Extracción realizada con éxito.")
+          }
+          })
+    }
+    })
+})
+
+
+// ------------------------------------------- DEPOSITO ------------------------------------------------//
+$("#confirmarextraccion_btn").click(function() {
+  creacion_movimiento = Math.floor(Date.now() / 1000)
+  importe = $("#cantidaddepositar_txt").val()
+  numero_cuenta = $("#cuentadepositar_txt").val()
+
+  if (!$.isNumeric(importe) || !$.isNumeric(cuenta)) {
+    $("#depositar_estado").text("El número contiene caracteres no-numericos.")
+    return
+  }
+
+  $.ajax({
+    url: "url",
+    type: "GET",
+    beforeSend: function() {
+      $("#depositar_estado").text("Cargando...")
+    },
+    success: function(response){
+        console.log(response)
+        cuenta = response
+        movimiento = {"creado": creacion_movimiento,
+        "procesado": Math.floor(Date.now() / 1000),
+        "tipo": 1,
+        "estado": 1,
+        "importe": importe,
+        "id_cuenta": numero_cuenta}
+
+        $.ajax({
+          url: "url",
+          data: movimiento,
+          type: "POST",
+          success: function(response){
+              console.log(response)
+              $("#depositar_estado").text("Deposito realizado con éxito.")
+          }
+          })
+    }
+    })
+})
+
+// ------------------------------------------- TRANSFERENCIA ------------------------------------------------//
+$("#confirmartransferencia_btn").click(function() {
+  creacion_movimiento = Math.floor(Date.now() / 1000)
+  importe = $("#cantidadtransferir_txt").val()
+  numero_cuenta_destino = $("#destinotransferir_txt").val()
+  numero_cuenta_origen = $("#origentransferir_txt").val()
+
+  if (!$.isNumeric(importe) || !$.isNumeric(numero_cuenta_origen) || !$.isNumeric(numero_cuenta_destino)) {
+    $("#depositar_estado").text("El número contiene caracteres no-numericos.")
+    return
+  }
+
+  $.ajax({
+    url: "url",
+    type: "GET",
+    beforeSend: function() {
+      $("#transferir_estado").text("Cargando...")
+    },
+    success: function(response){
+        console.log(response)
+        cuenta_origen = response
+        $.ajax({
+          url: "url",
+          type: "GET",
+          beforeSend: function() {
+            $("#transferir_estado").text("Cargando...")
+          },
+          success: function(response){
+              console.log(response)
+              cuenta_destino = response
+
+              movimiento = {"creado": creacion_movimiento,
+              "procesado": Math.floor(Date.now() / 1000),
+              "tipo": 0,
+              "estado": 1,
+              "importe": importe,
+              "id_cuenta": numero_cuenta_origen}
+
+              $.ajax({
+                url: "url",
+                data: movimiento,
+                type: "POST",
+                success: function(response){
+                    console.log(response)
+                    movimiento = {"creado": creacion_movimiento,
+                    "procesado": Math.floor(Date.now() / 1000),
+                    "tipo": 1,
+                    "estado": 1,
+                    "importe": importe,
+                    "id_cuenta": numero_cuenta_destino}
+
+                    $.ajax({
+                      url: "url",
+                      data: movimiento,
+                      type: "POST",
+                      success: function(response){
+                          console.log(response)
+                          $("#depositar_estado").text("Deposito realizado con éxito.")
+                      }
+                      })
+                }
+                })
+              }
+          })
+    }
+    })
+})
+
+// ------------------------------------------- RESUMEN ------------------------------------------------//
+$("#confirmarultimos_btn").click(function() {
+  numero_cuenta = $("#cuentaultimos_txt").val()
+
+  if (!$.isNumeric(numero_cuenta)) {
+    $("#ultimos_estado").text("El número contiene caracteres no-numericos.")
+    return
+  }
+
+  $.ajax({
+    url: "http://www.mocky.io/v2/5b2276372e00007e00e316cf",
+    type: "GET",
+    beforeSend: function() {
+      $("#ultimos_estado").text("Cargando...")
+    },
+    success: function(response){
+        console.log(response)
+        movimientos = response
+        for (i = 0; i < Math.min(response.length, 5); i++) {
+          $("#t" + (i + 1) + "1").html(response[i]["creado"])
+          $("#t" + (i + 1) + "2").html(response[i]["importe"])
+        }
+        $("#depositar_estado").text("Saldo recuperado.")
+    }
+    })
+})
